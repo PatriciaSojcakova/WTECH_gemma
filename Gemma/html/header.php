ab3e259c96af876dcd4a5ebb3829c1f325c5b87a
Pata <patka22922@gmail.com>
2025-03-25 00:08:33 +0100

    changing header

--- Gemma/html/header.php
+++ Gemma/html/header.php
@@ -3,19 +3,34 @@
     <div class="container-fluid">
         <div class="row w-100 d-flex align-items-center">
 
-            <div class="col text-start">
-                <a class="navbar-brand custom_italiana" href="#">Gemma</a>
+            <div class="col text-center">
+                <a class="navbar-brand custom_italiana fs-2 fs-md-2 fs-lg-1" href="#">Gemma</a>
             </div>
 
-            <div class="col text-center d-none d-sm-block">
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+            <div class="col-12 col-sm-10 col-md-8 col-lg-6">
+                <div class="search-container position-relative">
+                    <form class="d-flex align-items-center">
+                        <!-- Ikona vyhľadávania -->
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                             class="search-icon feather feather-search">
+                            <circle cx="11" cy="11" r="8"></circle>
+                            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                        </svg>
+
+                        <!-- Vstupné pole -->
+                        <input class="form-control search-input ps-5" type="search" placeholder="Search anything..." aria-label="Search">
+
+                        <!-- Tlačidlo na odoslanie -->
+                        <button class="btn btn-search ms-2" type="submit">Search</button>
+                    </form>
+                </div>
             </div>
 
-            <div class="col text-end">
-                <a class="navbar-brand custom_playfair" href="#">tools</a>
+            <div class="col text-end d-flex justify-content-center gap-3 gap-sm-4 gap-md-4">
+                <span class="material-icons" style="color: #000000;">account_circle</span>
+                <span class="material-icons" style="color: #000000;">favorite</span>
+                <span class="material-icons" style="color: #000000;">shopping_cart</span>
             </div>
 
         </div>

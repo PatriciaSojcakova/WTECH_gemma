b881e5c181055ebf67acbd3edada5e4995057622
Pata <patka22922@gmail.com>
2025-03-25 13:50:20 +0100

    sticky header

--- Gemma/html/main_desktop.php
+++ Gemma/html/main_desktop.php
@@ -5,10 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../css/custom.css">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
-
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=search" />
-
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=search"/>
     <title>Bootstrap demo</title>
 </head>
 <body>
@@ -16,29 +14,9 @@
         <?php include "../html/header.php" ?>
     </header>
 
-    <main class="container mt-4">
-        <!-- Regular content box -->
-        <div class="content-box">
-            <h3>Content Box Title</h3>
-            <p>This is just a simple content box. Below is a lot of content to make the page scrollable.</p>
-        </div>
+    <main class="container" style="padding-top: 100px;>
 
-        <!-- Extra long content to make the page scrollable -->
-        <div class="long-content">
-            <h3>Long Content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac vestibulum ligula. Mauris euismod diam sit amet massa dapibus, vel ultricies sapien vestibulum. Integer lacinia augue metus, sit amet tristique nisi varius a. Ut id varius turpis. Sed maximus egestas justo, ac facilisis justo blandit et. Nulla facilisi. Morbi auctor mi vitae diam fringilla, nec dapibus magna scelerisque. Etiam nec felis ac nisi suscipit feugiat.</p>
-            <p>Curabitur tincidunt dui ac lectus laoreet, sit amet volutpat erat vehicula. Donec tincidunt justo at bibendum interdum. Nunc ac malesuada elit. Nam ut lorem eu nisi aliquam posuere. Mauris convallis elit augue, non dictum eros volutpat in. Nullam tempor, lorem vel interdum posuere, lectus felis congue urna, sed volutpat nisi urna id metus. Fusce ac sem hendrerit, malesuada erat vel, tincidunt erat. Vivamus at mi at lorem interdum tempus eget at turpis.</p>
-            <p>Donec tempor tempor purus, et volutpat libero euismod vel. Etiam auctor tortor ut urna fermentum, vitae accumsan sapien auctor. Sed varius, ligula vel elementum gravida, ipsum sem dictum lorem, non euismod lorem odio eget velit. Proin ac erat eu sem consequat consequat. Curabitur sit amet nisi ac lacus tempor posuere et id libero. Integer ac massa libero. Fusce interdum nec purus in luctus.</p>
-            <p>Fusce nec lorem vitae ipsum dictum viverra sit amet a libero. Cras sit amet consectetur eros. Cras vel velit lectus. Aenean ac eros quis magna feugiat sodales. Integer ut laoreet ex. Nunc suscipit magna sed velit dictum, a vulputate nulla tincidunt. Suspendisse potenti. Sed viverra magna sed felis fermentum, a tincidunt arcu eleifend. Nam non felis odio. Fusce tristique mi ac odio fringilla fermentum.</p>
-            <p>Nullam fringilla neque non venenatis suscipit. Aliquam tincidunt libero eu sem hendrerit varius. Nulla sit amet velit dui. Vestibulum aliquet feugiat erat, non convallis lorem tempor sit amet. Nunc vel mi a mi dictum mollis. Integer sit amet consectetur libero. Phasellus tincidunt ex sit amet orci condimentum, sit amet tempor turpis feugiat. Fusce nec tincidunt ex. Cras ullamcorper nisi ligula, ac tincidunt nisi tempor a. Sed tincidunt vel sem non feugiat.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac vestibulum ligula. Mauris euismod diam sit amet massa dapibus, vel ultricies sapien vestibulum. Integer lacinia augue metus, sit amet tristique nisi varius a. Ut id varius turpis. Sed maximus egestas justo, ac facilisis justo blandit et. Nulla facilisi. Morbi auctor mi vitae diam fringilla, nec dapibus magna scelerisque. Etiam nec felis ac nisi suscipit feugiat.</p>
-            <p>Curabitur tincidunt dui ac lectus laoreet, sit amet volutpat erat vehicula. Donec tincidunt justo at bibendum interdum. Nunc ac malesuada elit. Nam ut lorem eu nisi aliquam posuere. Mauris convallis elit augue, non dictum eros volutpat in. Nullam tempor, lorem vel interdum posuere, lectus felis congue urna, sed volutpat nisi urna id metus. Fusce ac sem hendrerit, malesuada erat vel, tincidunt erat. Vivamus at mi at lorem interdum tempus eget at turpis.</p>
-            <p>Donec tempor tempor purus, et volutpat libero euismod vel. Etiam auctor tortor ut urna fermentum, vitae accumsan sapien auctor. Sed varius, ligula vel elementum gravida, ipsum sem dictum lorem, non euismod lorem odio eget velit. Proin ac erat eu sem consequat consequat. Curabitur sit amet nisi ac lacus tempor posuere et id libero. Integer ac massa libero. Fusce interdum nec purus in luctus.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac vestibulum ligula. Mauris euismod diam sit amet massa dapibus, vel ultricies sapien vestibulum. Integer lacinia augue metus, sit amet tristique nisi varius a. Ut id varius turpis. Sed maximus egestas justo, ac facilisis justo blandit et. Nulla facilisi. Morbi auctor mi vitae diam fringilla, nec dapibus magna scelerisque. Etiam nec felis ac nisi suscipit feugiat.</p>
-            <p>Curabitur tincidunt dui ac lectus laoreet, sit amet volutpat erat vehicula. Donec tincidunt justo at bibendum interdum. Nunc ac malesuada elit. Nam ut lorem eu nisi aliquam posuere. Mauris convallis elit augue, non dictum eros volutpat in. Nullam tempor, lorem vel interdum posuere, lectus felis congue urna, sed volutpat nisi urna id metus. Fusce ac sem hendrerit, malesuada erat vel, tincidunt erat. Vivamus at mi at lorem interdum tempus eget at turpis.</p>
-            <p>Donec tempor tempor purus, et volutpat libero euismod vel. Etiam auctor tortor ut urna fermentum, vitae accumsan sapien auctor. Sed varius, ligula vel elementum gravida, ipsum sem dictum lorem, non euismod lorem odio eget velit. Proin ac erat eu sem consequat consequat. Curabitur sit amet nisi ac lacus tempor posuere et id libero. Integer ac massa libero. Fusce interdum nec purus in luctus.</p>
 
-        </div>
     </main>
 
     <footer>

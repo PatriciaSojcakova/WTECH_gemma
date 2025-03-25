261b5a54ac41ce85b1e7be4c0c7c635977360e40
MichaelaH3 <103862798+MichaelaH3@users.noreply.github.com>
2025-03-25 13:29:52 +0100

    kontakt

--- Gemma/html/kontakt.php
+++ Gemma/html/kontakt.php
@@ -15,50 +15,49 @@
 </header>
 
 <main class="container mt-5">
-    <h2 class="text-center mb-4">Kontakt</h2>
+    <h2 class="mb-4 custom_playfair_medium">Kontakt</h2>
 
 
     <div class="row">
 
         <div class = "col-md-6">
-            <!-- Sekcia Adresa -->
-            <div class="card mb-4 border-0">
-                <div class="card-body">
-                    <h4 class="card-title">Adresa</h4>
-                    <p>Názov firmy<br>Ulica 123, Mesto, PSČ<br>Slovensko</p>
-                </div>
-            </div>
 
             <!-- Sekcia Telefón -->
             <div class="card mb-4 border-0">
                 <div class="card-body">
-                    <h4 class="card-title">Telefón</h4>
-                    <p>+421 123 456 789</p>
+                    <h4 class="card-title custom_playfair_medium">Telefón</h4>
+                    <p class="custom_playfair_txt">+421 123 456 789</p>
                 </div>
             </div>
 
-            <!-- Sekcia E-mail -->
+            <!-- Sekcia Adresa -->
             <div class="card mb-4 border-0">
                 <div class="card-body">
-                    <h4 class="card-title">E-mail</h4>
-                    <p><a href="mailto:info@firma.sk">info@firma.sk</a></p>
+                    <h4 class="card-title custom_playfair_medium">Adresa</h4>
+                    <p class="custom_playfair_txt">Názov firmy<br>Ulica 123, Mesto, PSČ<br>Slovensko</p>
                 </div>
             </div>
-
 
         </div>
 
 
         <div class="col-md-6">
+            <!-- Sekcia E-mail -->
+            <div class="card mb-4 border-0">
+                <div class="card-body">
+                    <h4 class="card-title custom_playfair_medium">E-mail</h4>
+                    <p class="custom_playfair_txt">info@gemma.sk</p>
+                </div>
+            </div>
 
             <!-- Sekcia Sociálne siete -->
             <div class="card mb-4 border-0">
                 <div class="card-body">
-                    <h4 class="card-title">Sociálne siete</h4>
+                    <h4 class="card-title custom_playfair_medium">Sociálne siete</h4>
                     <p>
-                        <a href="#">Facebook</a> |
-                        <a href="#">Instagram</a> |
-                        <a href="#">Twitter</a>
+                        Facebook - GEMMA_jewelry<br>
+                        Instagram - GEMMA_jewerly<br>
+                        Twitter - GEMMA_jewerly
                     </p>
                 </div>
             </div>
@@ -71,8 +70,8 @@
         <div class="col-md-12 d-flex justify-content-center">
             <div class="card mb-4 border-0">
                 <div class="card-body text-center">
-                    <h4 class="card-title">Otváracie hodiny</h4>
-                    <p>Pondelok - Piatok: 9:00 - 18:00<br>Sobota: 10:00 - 14:00<br>Nedeľa: Zatvorené</p>
+                    <h4 class="card-title custom_playfair_medium">Otváracie hodiny</h4>
+                    <p class="custom_playfair_txt">Pondelok - Piatok: 9:00 - 18:00<br>Sobota: 10:00 - 14:00<br>Nedeľa: Zatvorené</p>
                 </div>
             </div>
         </div>

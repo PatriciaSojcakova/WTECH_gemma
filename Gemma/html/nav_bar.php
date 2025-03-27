4f0746adf58ae4597f598977fd182f66fb5acdf9
Pata <patka22922@gmail.com>
2025-03-27 11:02:07 +0100

    filter_innit

--- Gemma/html/nav_bar.php
+++ Gemma/html/nav_bar.php
@@ -9,66 +9,91 @@
             <ul class="navbar-nav w-75 justify-content-between">
 
                 <li class="nav-item dropdown">
-                    <a id="loginDropdown" class="nav-link active custom_playfair_bold dropdown-toggle m-0 p-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a id="loginDropdown" class="nav-link active custom_playfair_bold dropdown-toggle m-0 p-0"
+                       href="products_desktop.php"
+                       role="button"
+                       data-bs-toggle="dropdown"
+                       aria-expanded="false"
+                       onclick="window.location.href='products_desktop.php'">
                         PRSTENE
                     </a>
                     <ul class="dropdown-menu custom-dropdown" style="background-color: #E2CBCB;margin-top: 3px;" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Jednoduché</a></li>
-                        <li><a class="dropdown-item" href="#">Vrstviteľné</a></li>
-                        <li><a class="dropdown-item" href="#">Obrúčky</a></li>
-                        <li><a class="dropdown-item" href="#">S kamienkom</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Jednoduché</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Vrstviteľné</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Obrúčky</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">S kamienkom</a></li>
                     </ul>
                 </li>
 
                 <li class="nav-item dropdown">
-                    <a id="loginDropdown" class="nav-link active custom_playfair_bold dropdown-toggle m-0 p-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a id="loginDropdown" class="nav-link active custom_playfair_bold dropdown-toggle m-0 p-0"
+                       href="products_desktop.php"
+                       role="button"
+                       data-bs-toggle="dropdown"
+                       aria-expanded="false"
+                       onclick="window.location.href='products_desktop.php'">
                         NÁUŠNICE
                     </a>
                     <ul class="dropdown-menu custom-dropdown" style="background-color: #E2CBCB;margin-top: 3px;" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Puzetkové náušnice</a></li>
-                        <li><a class="dropdown-item" href="#">Kruhové náušnice</a></li>
-                        <li><a class="dropdown-item" href="#">Perlové náušnice</a></li>
-                        <li><a class="dropdown-item" href="#">Visiace náušnice</a></li>
-                        <li><a class="dropdown-item" href="#">Retiazkové náušnice</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Puzetkové náušnice</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Kruhové náušnice</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Perlové náušnice</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Visiace náušnice</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Retiazkové náušnice</a></li>
                     </ul>
                 </li>
 
                 <li class="nav-item dropdown">
-                    <a id="loginDropdown" class="nav-link active custom_playfair_bold dropdown-toggle m-0 p-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        NÁHRDELNÍKY
+                    <a id="loginDropdown" class="nav-link active custom_playfair_bold dropdown-toggle m-0 p-0"
+                       href="products_desktop.php"
+                       role="button"
+                       data-bs-toggle="dropdown"
+                       aria-expanded="false"
+                       onclick="window.location.href='products_desktop.php'">
+                       NÁHRDELNÍKY
                     </a>
                     <ul class="dropdown-menu custom-dropdown" style="background-color: #E2CBCB;margin-top: 3px;" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Retiazkové náhrdelníky</a></li>
-                        <li><a class="dropdown-item" href="#">S príveskom</a></li>
-                        <li><a class="dropdown-item" href="#">Perlové náhrdelníky</a></li>
-                        <li><a class="dropdown-item" href="#">Tenisové náhrdelníky</a></li>
-                        <li><a class="dropdown-item" href="#">Chokers</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Retiazkové náhrdelníky</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">S príveskom</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Perlové náhrdelníky</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Tenisové náhrdelníky</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Chokers</a></li>
                     </ul>
                 </li>
 
                 <li class="nav-item dropdown">
-                    <a id="loginDropdown" class="nav-link active custom_playfair_bold dropdown-toggle m-0 p-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a id="loginDropdown" class="nav-link active custom_playfair_bold dropdown-toggle m-0 p-0"
+                       href="products_desktop.php"
+                       role="button"
+                       data-bs-toggle="dropdown"
+                       aria-expanded="false"
+                       onclick="window.location.href='products_desktop.php'">
                         NÁRAMKY
                     </a>
                     <ul class="dropdown-menu custom-dropdown" style="background-color: #E2CBCB;margin-top: 3px;" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Pevné náramky</a></li>
-                        <li><a class="dropdown-item" href="#">Retiazkové náramky</a></li>
-                        <li><a class="dropdown-item" href="#">Tenisové náramky</a></li>
-                        <li><a class="dropdown-item" href="#">Bezpríveskové</a></li>
-                        <li><a class="dropdown-item" href="#">Perlové náramky</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Pevné náramky</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Retiazkové náramky</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Tenisové náramky</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Bezpríveskové</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Perlové náramky</a></li>
                     </ul>
                 </li>
 
                 <li class="nav-item dropdown">
-                    <a id="loginDropdown" class="nav-link active custom_playfair_bold dropdown-toggle m-0 p-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        KOLEKCIE
+                    <a id="loginDropdown" class="nav-link active custom_playfair_bold dropdown-toggle m-0 p-0"
+                       href="products_desktop.php"
+                       role="button"
+                       data-bs-toggle="dropdown"
+                       aria-expanded="false"
+                       onclick="window.location.href='products_desktop.php'">
+                       KOLEKCIE
                     </a>
                     <ul class="dropdown-menu custom-dropdown" style="background-color: #E2CBCB;margin-top: 3px;" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Minimalistic</a></li>
-                        <li><a class="dropdown-item" href="#">Eternal</a></li>
-                        <li><a class="dropdown-item" href="#">GEMMA moments</a></li>
-                        <li><a class="dropdown-item" href="#">GEMMA x Oasis</a></li>
-                        <li><a class="dropdown-item" href="#">Signature of love</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Minimalistic</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Eternal</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">GEMMA moments</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">GEMMA x Oasis</a></li>
+                        <li><a class="dropdown-item" href="products_desktop.php">Signature of love</a></li>
                     </ul>
                 </li>
 

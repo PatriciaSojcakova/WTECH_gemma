9b0de19c961b7ebcb7a8ed4d515295c3ba1f60a4
Pata <patka22922@gmail.com>
2025-03-27 14:13:45 +0100

    product_filter_done

--- Gemma/html/products_desktop.php
+++ Gemma/html/products_desktop.php
@@ -16,56 +16,135 @@
 
     <?php include "../html/nav_bar.php"?>
 
-    <main class="container" style="padding-top: 86px;">
+    <main class="container" style="padding-top: 90px;">
 
         <div class="container">
             <!-- Tlačidlo na otvorenie/zatvorenie filtrov -->
-            <button class="filter-btn" data-bs-toggle="collapse" data-bs-target="#filterCollapse" aria-expanded="false">
-                Filtre
+            <button class="filter-btn custom_playfair_bold" data-bs-toggle="collapse" data-bs-target="#filterCollapse" aria-expanded="false">
+                FILTRE
             </button>
 
             <!-- Filter sekcia -->
             <div id="filterCollapse" class="collapse mt-2">
                 <div class="filter-container">
-                    <div class="row text-center">
+                    <div class="row justify-content-center">
                         <!-- Materiál -->
-                        <div class="col-md-3">
-                            <h5>Materiál</h5>
-                            <div class="form-check"><input class="form-check-input" type="checkbox" name="material"> niečo</div>
-                            <div class="form-check"><input class="form-check-input" type="checkbox" name="material"> niečo</div>
-                            <div class="form-check"><input class="form-check-input" type="checkbox" name="material"> niečo</div>
+                        <div class="col-md-3 d-flex flex-column align-items-start">
+                            <p class="custom_playfair_bold mt-3 mb-1">MATERIÁL</p>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="material1" name="material">
+                                <label class="form-check-label custom_playfair_semi" for="material1">zlato</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="material2" name="material">
+                                <label class="form-check-label custom_playfair_semi" for="material2">biele/ružové zlato</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="material3" name="material">
+                                <label class="form-check-label custom_playfair_semi" for="material3">striebro</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="material4" name="material">
+                                <label class="form-check-label custom_playfair_semi" for="material4">titanium</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="material5" name="material">
+                                <label class="form-check-label custom_playfair_semi" for="material5">chirurgická oceľ</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="material6" name="material">
+                                <label class="form-check-label custom_playfair_semi" for="material6">iné</label>
+                            </div>
                         </div>
 
                         <!-- Farba -->
-                        <div class="col-md-3">
-                            <h5>Farba</h5>
-                            <div class="form-check"><input class="form-check-input" type="checkbox" name="farba"> niečo</div>
-                            <div class="form-check"><input class="form-check-input" type="checkbox" name="farba"> niečo</div>
-                            <div class="form-check"><input class="form-check-input" type="checkbox" name="farba"> niečo</div>
+                        <div class="col-md-3 d-flex flex-column align-items-start">
+                            <p class="custom_playfair_bold mt-3 mb-1">FARBA KAMEŇA</p>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="farba1" name="farba">
+                                <label class="form-check-label custom_playfair_semi" for="farba1">diamant</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="farba2" name="farba">
+                                <label class="form-check-label custom_playfair_semi" for="farba2">rubín</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="farba3" name="farba">
+                                <label class="form-check-label custom_playfair_semi" for="farba3">zafír</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="farba4" name="farba">
+                                <label class="form-check-label custom_playfair_semi" for="farba4">smaragd</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="farba4" name="farba">
+                                <label class="form-check-label custom_playfair_semi" for="farba4">ametyst</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="farba5" name="farba">
+                                <label class="form-check-label custom_playfair_semi" for="farba5">topás</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="farba6" name="farba">
+                                <label class="form-check-label custom_playfair_semi" for="farba6">perla</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="farba7" name="farba">
+                                <label class="form-check-label custom_playfair_semi" for="farba7">jantár</label>
+                            </div>
+
                         </div>
 
                         <!-- Určenie -->
-                        <div class="col-md-3">
-                            <h5>Určenie</h5>
-                            <div class="form-check"><input class="form-check-input" type="checkbox" name="urcenie"> Ženy</div>
-                            <div class="form-check"><input class="form-check-input" type="checkbox" name="urcenie"> Muži</div>
-                            <div class="form-check"><input class="form-check-input" type="checkbox" name="urcenie"> Deti</div>
-                            <div class="form-check"><input class="form-check-input" type="checkbox" name="urcenie"> Unisex</div>
+                        <div class="col-md-3 d-flex flex-column align-items-start">
+                            <p class="custom_playfair_bold mt-3 mb-1">URČENIE</p>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="urcenie1" name="urcenie">
+                                <label class="form-check-label custom_playfair_semi" for="urcenie1">ženy</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="urcenie2" name="urcenie">
+                                <label class="form-check-label custom_playfair_semi" for="urcenie2">muži</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="urcenie3" name="urcenie">
+                                <label class="form-check-label custom_playfair_semi" for="urcenie3">deti</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="urcenie4" name="urcenie">
+                                <label class="form-check-label custom_playfair_semi" for="urcenie4">unisex</label>
+                            </div>
                         </div>
 
                         <!-- Cena -->
-                        <div class="col-md-3">
-                            <h5>Cena</h5>
-                            <div class="form-check"><input class="form-check-input" type="checkbox" name="cena"> niečo</div>
-                            <div class="form-check"><input class="form-check-input" type="checkbox" name="cena"> niečo</div>
-                            <div class="form-check"><input class="form-check-input" type="checkbox" name="cena"> niečo</div>
+                        <div class="col-md-3 d-flex flex-column align-items-start">
+                            <p class="custom_playfair_bold mt-3 mb-1">CENA</p>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="cena1" name="cena">
+                                <label class="form-check-label custom_playfair_semi" for="cena1">< 50€</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="cena2" name="cena">
+                                <label class="form-check-label custom_playfair_semi" for="cena2">50€ - 100€</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="cena3" name="cena">
+                                <label class="form-check-label custom_playfair_semi" for="cena3">100€ - 500€</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="cena4" name="cena">
+                                <label class="form-check-label custom_playfair_semi" for="cena4">500€ - 1000€</label>
+                            </div>
+                            <div class="filter-form-check">
+                                <input class="form-check-input" type="checkbox" id="cena5" name="cena">
+                                <label class="form-check-label custom_playfair_semi" for="cena5">1000€ <</label>
+                            </div>
                         </div>
-
                     </div>
 
                     <!-- Tlačidlo na filtrovanie -->
                     <div class="text-center mt-4">
-                        <button class="btn btn-secondary">Filtruj</button>
+                        <button class="btn btn-secondary bg-black w-25">Filtruj</button>
                     </div>
                 </div>
             </div>

d67214bad8d257684e82f24cccd468d6bf00f237
MichaelaH3 <103862798+MichaelaH3@users.noreply.github.com>
2025-03-27 22:22:57 +0100

    admin_account_image_deletion

--- Gemma/html/admin_account.php
+++ Gemma/html/admin_account.php
@@ -21,7 +21,7 @@
 
     <div class="row">
         <div class="col-md-4">
-            <div class="card">
+            <div class="card mt-4">
                 <div class="card-header custom-card-header-bg text-black">
                     <h5>Pridať nový produkt</h5>
                 </div>
@@ -54,24 +54,58 @@
         </div>
 
         <div class="col-md-4">
-            <div class="card">
+            <div class="card mt-4">
                 <div class="card-header custom-card-header-bg text-black">
                     <h5>Upraviť produkt</h5>
                 </div>
                 <div class="card-body">
-                    <form>
+                    <form method="post" action="update_product.php" enctype="multipart/form-data">
                         <div class="mb-3">
                             <label for="editProductCode" class="form-label">Kód produktu</label>
-                            <input type="text" class="form-control" id="editProductCode" placeholder="Zadajte kód produktu na úpravu" required>
+                            <input type="text" class="form-control" name="product_code" id="editProductCode" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="editProductName" class="form-label">Názov produktu</label>
+                            <input type="text" class="form-control" name="product_name" id="editProductName" required>
                         </div>
                         <div class="mb-3">
                             <label for="editProductDescription" class="form-label">Popis produktu</label>
-                            <textarea class="form-control" id="editProductDescription" placeholder="Upravte popis produktu"></textarea>
+                            <textarea class="form-control" name="product_description" id="editProductDescription"></textarea>
                         </div>
                         <div class="mb-3">
                             <label for="editProductPrice" class="form-label">Cena</label>
-                            <input type="number" class="form-control" id="editProductPrice" placeholder="Upravte cenu produktu">
+                            <input type="number" class="form-control" name="product_price" id="editProductPrice">
                         </div>
+
+                        <div class="mb-3">
+                            <label class="form-label">Obrázky produktu</label>
+                            <div class="d-flex flex-wrap gap-2">
+                                <div class="text-center">
+                                    <div class="img-thumbnail d-flex justify-content-center align-items-center bg-secondary text-white" style="width: 100px; height: 100px;">
+                                        Obrázok tu
+                                    </div>
+                                    <input type="checkbox" name="delete_images[]" value="1"> Vymazať
+                                </div>
+                                <div class="text-center">
+                                    <div class="img-thumbnail d-flex justify-content-center align-items-center bg-secondary text-white" style="width: 100px; height: 100px;">
+                                        Obrázok tu
+                                    </div>
+                                    <input type="checkbox" name="delete_images[]" value="2"> Vymazať
+                                </div>
+                                <div class="text-center">
+                                    <div class="img-thumbnail d-flex justify-content-center align-items-center bg-secondary text-white" style="width: 100px; height: 100px;">
+                                        Obrázok tu
+                                    </div>
+                                    <input type="checkbox" name="delete_images[]" value="3"> Vymazať
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="editProductImages" class="form-label">Pridať nové obrázky</label>
+                            <input type="file" class="form-control" name="new_images[]" multiple accept="image/*">
+                        </div>
+
                         <button type="submit" class="btn btn-dark w-100">Upraviť produkt</button>
                     </form>
                 </div>
@@ -79,7 +113,7 @@
         </div>
 
         <div class="col-md-4">
-            <div class="card">
+            <div class="card mt-4">
                 <div class="card-header custom-card-header-bg text-black">
                     <h5>Odstrániť produkt</h5>
                 </div>

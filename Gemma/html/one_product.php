d5a61d6dc80523cf74c38aa8f015b72aea5777f1
MichaelaH3 <103862798+MichaelaH3@users.noreply.github.com>
2025-03-29 11:15:14 +0100

    titles

--- Gemma/html/one_product.php
+++ Gemma/html/one_product.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=search"/>
-    <title>Bootstrap demo</title>
+    <title>Detail produktu</title>
 </head>
 <body class="one_product">
 <header>

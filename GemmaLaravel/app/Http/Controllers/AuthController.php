23cd6170657c1fd32f27e7c5e7a9949cff6aca28
Pata <patka22922@gmail.com>
2025-04-20 22:03:26 +0200

    personal information update

--- GemmaLaravel/app/Http/Controllers/AuthController.php
+++ GemmaLaravel/app/Http/Controllers/AuthController.php
@@ -10,27 +10,5 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'surname' => 'required|string|max:255',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|min:6',
-            'conditions' => 'accepted',
-        ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'surname' => $request->surname,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-
-        Auth::login($user); // môžeš aj vynechať, ak ho nechceš hneď prihlásiť
-
-        return redirect('/personal_account');
-    }
-
-    // tvoje login() tu môže zostať
 }

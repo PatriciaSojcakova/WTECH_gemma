01f403faec7467153bedca14ec2e735f6bc17868
Pata <patka22922@gmail.com>
2025-04-22 19:26:09 +0200

    tables innit

---
+++ GemmaLaravel/database/migrations/2025_04_22_163619_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('id_product');
+            $table->string('name');
+            $table->string('description');
+            $table->string('material');
+            $table->string('stone_color');
+            $table->string('purpose');
+            $table->integer('quantity');
+            $table->float('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
